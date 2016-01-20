ebd9e2079c707e6284fffff85c9a9350fdce8830
Rogerio de Leon Pereira <vinashu@gmail.com>
2016-01-20 15:29:23 -0200

    Adicionado typeValue

--- primitive.php
+++ primitive.php
@@ -1,16 +1,25 @@
 <?php 
 header("Content-Type: text/html; charset=UTF-8",true);
 class Primitive {
+    //valor da primitiva
     private $value;
+    //descritor do tipo da primitiva
     private $type;
+    //código do tipo da primitiva
+    private $typeValue;
+    //retorno padrão para geração de SQL
     private $sql;
+    //constantes de codificação do tipo de primitiva
     const INTEGER = 1;
     const STRING = 2;
     const FLOAT = 3;
     const BOOL = 4;
+    const UNDEFINED = 9;    
+    //construtor
     public function __construct($value = ""){
         $this->value = $value;
         $this->setType();
+        $this->setTypeValue();
     }
     public function getValue(){
         return $this->value;
@@ -26,7 +35,15 @@ class Primitive {
             ($this->isInteger() ? 'Integer' : 
                 ($this->isFloat() ? 'Float' :
                     ($this->isBool() ? 'Bool' :'Undefined')));
+                    
     }
+    private function setTypeValue(){      
+        $this->typeValue = $this->isString() ? self::STRING : 
+            ($this->isInteger() ? self::INTEGER : 
+                ($this->isFloat() ? self::FLOAT :
+                    ($this->isBool() ? self::BOOL : self::UNDEFINED)));
+                    
+    }    
     public function isInteger(){
         return is_int($this->value);
     }
